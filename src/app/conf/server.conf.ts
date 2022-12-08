fd4b3c87e7b737d118adc6817b6a9f3ea92dd5fe
Mite Gjorgjiev <43971504+mitinoh@users.noreply.github.com>
2022-12-08 15:38:20 +0100

    Update server.conf.ts

--- src/app/conf/server.conf.ts
+++ src/app/conf/server.conf.ts
@@ -1,7 +1,5 @@
 import { environment } from 'src/environments/environment';
 
-
-// configurazione di h4
 //const protocol = environment.production ? location.protocol : 'https'
 //const njHost = environment.production ? location.hostname : '107.174.186.223.nip.io'
 //const njPort = environment.production ? location.port : 443
